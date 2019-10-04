21aeff6a1329d64b9ea7b6fa08c7df0391db1899
LordRayd <samuel-leb@hotmail.fr>
2019-10-04 09:56:39 +0200

    Update tp3_squelette.cpp

--- 2110/tp3_squelette.cpp
+++ 2110/tp3_squelette.cpp
@@ -43,6 +43,13 @@ void display(void)
     glVertex3f(0,_height,0);
     glEnd();
 
+    drawPolygon();
+
+  
+    glFlush();
+}
+
+void drawPolygon(){
     glColor3f(1.0,1.0,1.0);
     glBegin(GL_POLYGON);
     glVertex3f(2,3,0);
@@ -51,9 +58,6 @@ void display(void)
     glVertex3f(4,3,0);
     glTranslatef(1.4,1.4,1.4);
     glEnd();
-
-  
-    glFlush();
 }
 
 void reshape(int w, int h)
@@ -106,8 +110,11 @@ void keyboard(unsigned char key, int x, int y)
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); 
             display();
             break;
-        case 's' : 
+        case 's' :
+            glLoadIdentity();
+            glMatrixMode(GL_MODELVIEW);
             glTranslatef(1.4,1.4,1.4);
+            drawPolygon();
             break;
         case 'r' : 
             glRotatef(1.4,1.4,1.4);
