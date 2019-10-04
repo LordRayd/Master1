6e145506fbbd25420360b27ef02727510744a19a
LordRayd <samuel-leb@hotmail.fr>
2019-10-04 10:41:01 +0200

    Update tp3_squelette.cpp

--- 2110/tp3_squelette.cpp
+++ 2110/tp3_squelette.cpp
@@ -41,6 +41,7 @@ void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
     
+    glLoadIdentity();
     glColor3f(1.0, 0.0, 0.0);
     glLineWidth(2.0);
     glBegin(GL_LINES); //Draws x-axis
@@ -48,6 +49,7 @@ void display(void)
     glVertex3f(_width,0,0);
     glEnd();
 
+    glLoadIdentity();
     glColor3f(0.0, 1.0, 0.0);
     glBegin(GL_LINES); //Draws y-axis
     glVertex3f(0,-_height,0);
