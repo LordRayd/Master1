27330713d61439b9b8f444b0c891c7878a7afbc4
LordRayd <samuel-leb@hotmail.fr>
2019-10-05 09:53:37 +0200

    Update Launcher.java

--- 2162/TP3/Launcher.java
+++ 2162/TP3/Launcher.java
@@ -68,29 +68,10 @@ public class Launcher {
 			/*System.out.println("Voisin de : Ranelagh");
 			String[] voisins = Reseau.stationsVoisinesDe(reseau,"Ranelagh");
 			Arrays.stream(voisins).forEach(voisin -> System.out.println(Reseau.modifierNom(voisin)));*/
-			/*System.out.println("Nombre de stations : "+reseau.getStation().size());
-			reseau.getStation().stream().sorted(Comparator.comparing(Station::getNom)).forEach((st) -> System.out.println(st.getNom()));*/
+			System.out.println("Nombre de stations : "+reseau.getStation().size());
+			reseau.getStation().stream().sorted(Comparator.comparing(Station::getNom)).forEach((st) -> System.out.println(st.getNom()));
 			//Valeur de 20 pour la pile de recherche
 			//pile plus grand que chemin arret
-			String accents 	= "È,É,Ê,Ë,Û,Ù,Ï,Î,À,Â,Ô,è,é,ê,ë,û,ù,ï,î,à,â,ô,Ç,ç,Ã,ã,Õ,õ";
-			String expected	= "E,E,E,E,U,U,I,I,A,A,O,e,e,e,e,u,u,i,i,a,a,o,C,c,A,a,O,o";
-			
-			String accents2	= "çÇáéíóúýÁÉÍÓÚÝàèìòùÀÈÌÒÙãõñäëïöüÿÄËÏÖÜÃÕÑâêîôûÂÊÎÔÛ";
-			String expected2	= "cCaeiouyAEIOUYaeiouAEIOUaonaeiouyAEIOUAONaeiouAEIOU";
-			
-			String accents3	= "Gisele Bündchen da Conceição e Silva foi batizada assim em homenagem à sua conterrânea de Horizontina, RS.";
-			String expected3	= "Gisele Bundchen da Conceicao e Silva foi batizada assim em homenagem a sua conterranea de Horizontina, RS.";
-			String accents4	= "/Users/rponte/arquivos-portalfcm/Eletron/Atualização_Diária-1.23.40.exe";
-			String expected4	= "/Users/rponte/arquivos-portalfcm/Eletron/Atualizacao_Diaria-1.23.40.exe";
-			
-			System.out.println(Reseau.unaccent(accents));
-			System.out.println(Reseau.unaccent(accents2));
-			System.out.println(Reseau.unaccent(accents3));
-			System.out.println(Reseau.unaccent(accents4));
-				/*assertEquals(expected2, StringUtils.unaccent(accents2));
-				assertEquals(expected3, StringUtils.unaccent(accents3));
-				assertEquals(expected4, StringUtils.unaccent(accents4));*/
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
