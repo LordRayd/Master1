da7c26d711ce1879c2e42193c691837568cda45a
LordRayd <samuel-leb@hotmail.fr>
2019-10-07 18:25:18 +0200

    Update Launcher.java

--- 2162/TP3/Launcher.java
+++ 2162/TP3/Launcher.java
@@ -70,12 +70,12 @@ public class Launcher {
 			String[] voisins = Reseau.stationsVoisinesDe(reseau,"Ranelagh");
 			Arrays.stream(voisins).forEach(voisin -> System.out.println(Reseau.modifierNom(voisin)));*/
 			//System.out.println("Nombre de stations : "+reseau.getStation().size());
-			//reseau.getStation().stream().sorted(Comparator.comparing(Station::getNom)).forEach((st) -> System.out.println(st.getNom()));
-			Collection<Trajet> chemin = Reseau.cheminDeVers(reseau,new Station("chateaudevincennes"), new Station("berault"));
+			reseau.getStation().stream().sorted(Comparator.comparing(Station::getNom)).forEach((st) -> System.out.println(st.getNom()));
+			/*Collection<Trajet> chemin = Reseau.cheminDeVers(reseau,new Station("chateaudevincennes"), new Station("berault"));
 			System.out.println(chemin.size());
 			for(Trajet tr : chemin){
 				System.out.println(tr.toString());
-			}
+			}*/
 			//Valeur de 20 pour la pile de recherche
 			//pile plus grand que chemin arret
 		} catch (IOException e) {
