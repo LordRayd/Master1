9f302749694e9127f958cfbedc5ed1b8b9cd31a7
LordRayd <samuel-leb@hotmail.fr>
2019-10-13 20:06:23 +0200

    a

--- 2162/TP4/Anagramme.scala
+++ 2162/TP4/Anagramme.scala
@@ -7,7 +7,8 @@ object Anagramme {
         println("Entrez le mot dont vous souhaitez trouver les anagrammes")
         val search = scala.io.StdIn.readLine()
         val result = trouveAnagrammesRec(search)
-        println("Nombbre de mot : " + result.size)
+        //val result = anagrammeDe(search)
+        println("Nombre de mot : " + result.size)
         println("Afficher tout les nombres (y/n)")
         val affiche = scala.io.StdIn.readChar()
         if(affiche == 'y'){
@@ -17,12 +18,16 @@ object Anagramme {
 
     }
 
-    def trouveAnagrammesRec(mot: String): HashSet[String] = {
+    def anagrammeDe(mot_ : String): HashSet[String] = {
+        return mot_.permutations.toSet
+    }
+
+    def trouveAnagrammesRec(mot_ : String): HashSet[String] = {
         var retour : HashSet[String] = new HashSet[String]()
 
         var nb = 0
-        for(c <- mot){
-            var sb = new StringBuilder(mot);
+        for(c <- mot_){
+            var sb = new StringBuilder(mot_);
             var motRestant = sb.deleteCharAt(nb).toString()
             if(motRestant.length < 1){
                 retour += s"$c$motRestant"
---
+++ 2162/TP4/README.txt
@@ -0,0 +1,16 @@
+OS Utilisé:
+    Windows 10
+
+Environnement Scala:
+    Scala compiler version 2.13.1 -- Copyright 2002-2019, LAMP/EPFL and Lightbend, Inc.
+    Scala code runner version 2.13.1 -- Copyright 2002-2019, LAMP/EPFL and Lightbend, Inc.
+
+Pour lancer le programme, il vous faudra d'abord le compiler avec la commande:
+    scalac ./Anagramme.scala
+Puis vous pourrez le lancer avec la commande
+    scala Anagramme
+
+Déroulement de l'exécution:
+Le programme vous demandera de rentrer un mot (ou une phrase) de votre choix.
+Il vous affichera ensuite le nombre d'anagramme réalisable à l'aide des lettres de ce mot.
+Pour tous les voir s'afficher vous devrez entrer le caractère 'y', tout autre caractère mettra fin au programme.
