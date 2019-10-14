6e2d1b6b6c33763c65ae77068e756442c5460d6f
LordRayd <samuel-leb@hotmail.fr>
2019-10-14 15:15:33 +0200

    a

---
+++ 2163/TP4/TP4.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io.wavfile import read
+from scipy.fftpack import fft
+from scipy import signal
+
---
+++ 2163/TP4/fft-oiseaux.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 19 14:31:07 2017
+
+@author: sylviegibet
+
+"""
+
+# Saisies 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io.wavfile import read
+from scipy.fftpack import fft
+from scipy import signal
+
+#(fs,x) = read('Sons/Oiseaux/etourneau.wav')
+#(fs,x) = read('Sons/Oiseaux/fauvette.wav')
+#(fs,x) = read('Sons/Oiseaux/piebavarde.wav')
+(fs,x) = read('Sons/Oiseaux/rossignol.wav')
+#(fs,x) = read('Sons/Classique/nocturne_chopin.wav')
+
+
+print('taille du fichier : ', x.size)
+print('fréquence d\'échantillonnage : ', fs)
+print('durée du signal : ', x.size/fs, 's')
+
+#Nf=4096
+#offset=256   
+
+def tffAmplitude(Nf,offset):
+    sf = np.zeros(Nf)
+    sf[:]=x[offset:offset+Nf]
+    X =fft(sf)/Nf
+    F =np.linspace(0,fs,Nf)	
+    mX = 2*abs(X)
+    powerSpectra = 10 * np.log10(abs(X))
+    pX=np.angle(X)
+    print(np.size(mX))
+    
+    fig1 = plt.figure()
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Amplitude')
+    F1=F[offset:1024+offset]
+    #mX1=mX[256:1024+256]
+    mX1=powerSpectra[offset:1024+offset]
+    plt.plot(F1,mX1)
+    
+def tffPhase(Nf,offset):
+    fig2 = plt.figure()
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Phase')
+    F1=F[100:offset]
+    pX1=pX[100:offset]
+    plt.plot(F1,pX1)
+    
+def tffSpectre(NF,offset):
+    fig3 = plt.figure()
+    f, t, Sxx = signal.spectrogram(x, fs)
+    plt.pcolormesh(t, f, np.log(Sxx))
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    
+tffAmplitude(4096,512)
+tffPhase(4096,512)
+tffSpectre(4096,512)
+
+def calculerSpectre(Nf, offset, winSize):
+    sf = np.zeros(Nf)
+    sf[:]=x[offset:offset+Nf]
+    X =fft(sf)/Nf
+    F =np.linspace(0,fs,Nf)	
+    mX = 2*abs(X)
+    powerSpectra = 10 * np.log10(abs(X))
+    pX=np.angle(X)
+    print(np.size(mX))
+    
+    fig1 = plt.figure()
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Amplitude')
+    
+    for i in range(offset,winSize+offset):
+        
+    
