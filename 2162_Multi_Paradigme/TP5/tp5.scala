a52b9a08477522a6272a105ecf3781f1e506cd6c
LordRayd <samuel-leb@hotmail.fr>
2019-10-18 15:55:03 +0200

    a

--- 2162_Multi_Paradigme/TP5/tp5.scala
+++ 2162_Multi_Paradigme/TP5/tp5.scala
@@ -1,4 +1,5 @@
 import scala.io.AnsiColor
+import scala.reflect.ClassTag
 
 object Ansi{
 
@@ -26,13 +27,60 @@ object Ansi{
     def white: String = "\u001B[37m"
     def fWhite: String = "\u001B[47m"
 
-    def reset : String = fBlack + white
+    def reset : String = "\u001B[0m"
 }
+
+class Echiquier[Piece : ClassTag](cote_  :  Int){
+
+    var plateau : Array[Array[Option[Piece]]] = Array.ofDim[Option[Piece]](cote_,cote_)
+
+    def this() = this(8)
+
+    def placerEn(piece_ : Option[Piece], x_ : Int, y_ : Int) : Unit = this.plateau(x_).update(y_,piece_)
+
+    def update(coupleXY_ : Tuple2[Int,  Int], piece_ : Option[Piece]): Unit = placerEn(piece_, coupleXY_._1, coupleXY_._2)
+
+    def apply(x_ : Int, y_ : Int) : Piece = {
+        this.plateau(x_)(y_)
+    }
+
+    def apply(coupleXY_ : Tuple2[Int, Int]) : Piece = {
+        this.plateau(coupleXY_._1)(coupleXY_._2)
+    }
+}
+
+trait Piece {
+
+}
+
+class PieceCol(etiquette_ :  String,  codeAnsi_ :  String){
+    private var etiquette : String = etiquette_
+    private var codeAnsi : String = codeAnsi_
+    def this(etiquette_ : String) = this(etiquette_, Ansi.fBlack + Ansi.white)
+    override def toString : String =  codeAnsi_ + etiquette_ + Ansi.reset
+}
+
+object PieceCol{
+    def apply(etiquette_ : String, codeAnsi_ : String ) : PieceCol = new PieceCol(etiquette_, codeAnsi_)
+    def apply(etiquette_ : String ) : PieceCol = new PieceCol(etiquette_)
+}
+
+
 
 object tp5 {
 
     def main(args_ : Array[String] ):Unit = {
         println("coucou"+Ansi.fBlue+Ansi.red+" abcdef "+Ansi.reset)
+        val cavalier = PieceCol("caval", Ansi.red)      
+        println(cavalier) 
+        //var echiquier = new Echiquier(8)
+        //echiquier.placerEn(piece,3,3)
 
+        var valeur: Option[Int] = Some(2)     
+        println(valeur) // donne Some(2)     
+        val Some(combien) = valeur     
+        println(combien) // donne 2     
+        valeur = None     
+        println(valeur) // donne None
     }
 }
