242b1f77c111965a54c21f7fc620251ca98d5b90
LordRayd <samuel-leb@hotmail.fr>
2019-10-25 08:15:37 +0200

    a

---
+++ 2110_Interface_Graphique/TP6/libQGLViewer-2.7.1/examples/TP5/tp4_exo1.cpp
@@ -0,0 +1,6 @@
+#include "tp4_exo1.h"
+
+tp4_exo1::tp4_exo1()
+{
+
+}
---
+++ 2110_Interface_Graphique/TP6/libQGLViewer-2.7.1/examples/TP5/tp4_exo1.h
@@ -0,0 +1,11 @@
+#ifndef TP4_EXO1_H
+#define TP4_EXO1_H
+
+
+class tp4_exo1
+{
+public:
+    tp4_exo1();
+};
+
+#endif // TP4_EXO1_H
