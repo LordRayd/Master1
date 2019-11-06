8e9d10fb051bb54b4182600ec40e359ed294698b
LordRayd <samuel-leb@hotmail.fr>
2019-11-06 08:53:13 +0100

    Update LeBerreSamuel.c

--- 2160_Concurrences_Systemes/TP_CONTROLE/LeBerreSamuel.c
+++ 2160_Concurrences_Systemes/TP_CONTROLE/LeBerreSamuel.c
@@ -15,11 +15,108 @@
 #include <string.h>
 
 #define MUTEX 0
+#define NB_ESCLAVE 5
+#define NB_ELEMENT 10
 
 key_t cle; /* cle ipc */
 
 int semid; /* nom local de l'ensemble des semaphores */
 
+int shmid;
+
+int * tab;
+
+struct sembuf op;
+
+void maitre(){
+    char c;
+    unsigned int rep;
+    unsigned int delai;
+    while ( scanf(" %c%u%u",&c, &rep,&delai) == 3){
+        
+        printf("P(&MUTEX)\n");
+        op.sem_num=MUTEX;op.sem_op=-1;op.sem_flg=0;
+        semop(semid,&op,1);
+
+        
+        if(*(tab  = (int*) shmat(shmid,NULL,0)) == -1){
+            perror("probleme shmat");
+            exit(4);
+        }
+
+        int pos = *tab;
+
+        tab = tab + sizeof(int) + pos*(2*sizeof(int)+sizeof(char));
+
+        *tab = (int)c;
+        tab = tab + sizeof(char);
+        *tab = rep;
+        tab = tab + sizeof(int);
+        *tab = delai;
+
+        if(shmdt(tab) == -1){
+            perror("probleme sur shmdt");
+            exit(4);
+        }
+
+        printf("V(&MUTEX)\n");
+        op.sem_num=MUTEX;op.sem_op=1;op.sem_flg=0;
+        semop(semid,&op,1);
+
+
+    }
+}
+
+void esclave(){
+
+    int x;
+    for(x=0;x<5;x++){
+        write(1,4,1);
+        sched_yield();
+        sleep(1);
+    }
+}
+
 int main(int argc, char * argv[]){
-    
+
+    //init sem
+    ushort init_sem[1]={10};
+
+    // creation d'une cle IPC en fonction du nom du programme
+    if ((cle=ftok(argv[0],'0')) == -1 ) {
+        fprintf(stderr,"Probleme sur ftoks\n");
+        exit(1);
+    }
+
+    // demande un ensemble de semaphore (ici deux mutex)
+    if ((semid=semget(cle,1,IPC_CREAT|0666))==-1) {
+        fprintf(stderr,"Probleme sur semget\n");
+        exit(2);
+    }
+
+    // initialise l'ensemble
+    if (semctl(semid,1,SETALL,init_sem)==-1) {
+        fprintf(stderr,"Probleme sur semctl SETALL\n");
+        exit(3);
+    }
+
+    //Allocation espace necessaire tab
+    if((shmid = shmget(cle, sizeof(int)+NB_ELEMENT*(sizeof(char)+sizeof(int)*2), IPC_CREAT | 0666)) == -1){
+        perror("shmget");
+        exit(1);
+    }
+
+
+    int i =0;
+    int pid;
+    while(i<NB_ESCLAVE){
+        if((pid = fork()) == 0){
+            esclave();
+            exit(0);
+        }
+    }
+    maitre();
+
+
+
 }
