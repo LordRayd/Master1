f75b194d07589bf92efeec009da01534b8f00693
LordRayd <samuel-leb@hotmail.fr>
2019-11-06 21:08:45 +0100

    Update tp6.scala

--- 2162_Multi_Paradigme/TP6/tp6.scala
+++ 2162_Multi_Paradigme/TP6/tp6.scala
@@ -142,7 +142,7 @@ class CavalierEuler(cote_ : Int = 8){
             var positionPossible = trouveDeplacementsCavalier(xy_)
             for( pos <- positionPossible){
                 modele(pos._1)(pos._2) = etape_
-                if(etape_ == (( cote_ * cote_ ) - 60)){
+                if(etape_ == (( cote_ * cote_ ) - 63)){
                     return true;
                 }
                 if(trouvePositions(pos, etape_ + 1)){
