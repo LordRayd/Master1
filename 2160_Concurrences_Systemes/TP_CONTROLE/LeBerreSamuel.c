201acecc9988691fc600d6d56ae21abc0608ca99
LordRayd <samuel-leb@hotmail.fr>
2019-11-06 08:14:29 +0100

    Update LeBerreSamuel.c

--- 2160_Concurrences_Systemes/TP_CONTROLE/LeBerreSamuel.c
+++ 2160_Concurrences_Systemes/TP_CONTROLE/LeBerreSamuel.c
@@ -1,7 +1,24 @@
 /**
  * autheur Samuel LE BERRE
  */
+#include <unistd.h> // appel systeme fork
+#include <fcntl.h>  // appel system unix ES
+#include <stdio.h> // librairie standard C
+#include <stdlib.h> // exit
+#include <sched.h>  // sche_yield
+#include <sys/types.h>
+#include <sys/sem.h> // semaphore IPC
+#include <sys/ipc.h> // services IPC
+#include <sys/wait.h> // wait
+#include <time.h>
+#include <sys/stat.h>
+#include <string.h>
+
 #define MUTEX 0
+
+key_t cle; /* cle ipc */
+
+int semid; /* nom local de l'ensemble des semaphores */
 
 int main(int argc, char * argv[]){
     
