a2084ddb656d4a7c1f53c204ce9ee1ebfb405dd7
lordrayd <samuel-leb@hotmail.fr>
2019-11-12 16:23:44 +0100

    create TP8

---
+++ 2163_Traitement_Numerique/TP8/TP8.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+import pandas as pd   #pour l’exploration de données
+import numpy as np    #pour les opérations numériques
