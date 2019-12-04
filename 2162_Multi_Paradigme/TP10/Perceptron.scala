440a1d8ab6958948c1a1f79d32ad75f31e0940f1
LordRayd <samuel-leb@hotmail.fr>
2019-12-04 15:00:49 +0100

    td10

--- 2162_Multi_Paradigme/TP10/Perceptron.scala
+++ 2162_Multi_Paradigme/TP10/Perceptron.scala
@@ -1,6 +1,6 @@
 import scala.{Array => $} 
 
-class Perceptron(couches_ : Array[Int){
+class Perceptron(couches_ : Array[Int]){
 
     // pour rester très proche du cours   
     var outputI = (0 until couches_.length).map( i => new Array[Double](couches_(i))).toArray  
@@ -14,18 +14,18 @@ class Perceptron(couches_ : Array[Int){
     def fp(x_ : Double) : Double = f(x_)*(1-f(x_))
 
     // produit scalaire   
-    def prod(p1_: Array[Double], p2_: Array[Double]) : Double = {     
+    def prod(p1_ : Array[Double], p2_ : Array[Double]) : Double = {     
         require(p1_.length == p2_.length, "pour le produit les vecteurs doivent avoir la même taille")     
         p1_.zip(p2_).map{ case (a,b) => a*b }.sum   
     }
 
     // calcul d'erreur quadratique   
-    def errQuad(p1_: Array[Double], p2_:Array[Double]) : Double = {     
+    def errQuad(p1_ : Array[Double], p2_ : Array[Double]) : Double = {     
         require(p1_.length == p2_.length, "pour l'erreur quadratique les vecteurs doivent avoir la même taille")     
         p1_.zip(p2_).map{ case (a,b) => (a-b)*(a-b)}.sum   
     }
 
-    def poidsHasard() { // met au hasard les poids du réseau     
+    def poidsHasard() : Unit = { // met au hasard les poids du réseau     
         for( c <- poids.indices)  // chaque couche       
             for (n <- poids(c).indices)  // chaque neurone         
                 for(p <- poids(c)(n).indices) 
@@ -42,11 +42,12 @@ class Perceptron(couches_ : Array[Int){
             outputI(0)(i) = in_(i)
         }
         
-        for( c <- poids.indices))  // chaque couche       
-            for (n <- poids(c).indices)  // chaque neurone        
+        for( c <- poids.indices)  // chaque couche       
+            for (n <- poids(c).indices)  // chaque neurone         
                 for(p <- poids(c)(n).indices) 
                     inputI(c)(p) += poids(c)(n)(p) * outputI(c-1)(n)
-            for()
+        
+        outputI(couches_.length - 1)
     }
 }
 
@@ -54,14 +55,15 @@ object Perceptron{
     def apply( couches_ : Int* ): Perceptron = {     
         new Perceptron( couches_.toArray )   
     }
-
-    
-
-    
+    def apply(couches_ : Array[Int]) : Perceptron = {
+        new Perceptron(couches_)
+    }
 }
 
 object Main {
     def main(args: Array[String]): Unit ={
-        val reponse = monPerceptron($(1.0, 2.0, 4.0))
+        val perceptron = Perceptron($(4, 1))
+        val reponse = perceptron($(1.0, 0.0))
+        println(reponse)
     }
 }
