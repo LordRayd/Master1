66a62aef2336a631b1e4affd5ec963dd256824f3
lordrayd <samuel-leb@hotmail.fr>
2020-03-05 15:16:37 +0100

    tp6 fin

--- 2244_Programmation_Web/TP6/js/script.js
+++ 2244_Programmation_Web/TP6/js/script.js
@@ -1,22 +1,30 @@
-$(document).ready(function() {
-    $(".central").click(function() {
-        $(".volet").animate({ width: '100px' });
-        $(this).animate({ width: '100px' });
-    });
+$(function() {
 
-    $(".volet").click(function() {
-        $(".central").animate({ width: '100px' });
-        $(".central").addClass("imgG");
-        $(".central").addClass("volet");
-        $(".central").removeClass('central')
-        $(this).addClass("central")
-        $(this).removeClass("imgG")
-        $(this).removeClass("volet")
-
-        if ($(this).width() == 100) {
-            $(this).animate({ width: '470px' });
+    $(".volet").on("click", function() {
+        if ($(this).hasClass('central')) {
+            if ($(this).width() == 470) {
+                $('.volet').each(function() {
+                    $(this).animate({ width: '100px' });
+                    $(this).find(".panneauTxt").hide();
+                });
+                $(this).addClass("imgG");
+                $(this).removeClass("central");
+            }
         } else {
-            $(this).animate({ width: '100px' });
+            $('.central').each(function() {
+                $(this).find(".panneauTxt").hide();
+                $(this).animate({ width: '100px' });
+                $(this).addClass("imgG");
+                $(this).removeClass("central");
+            });
+            $(this).find('.panneauTxt').css({
+                'top': $(this).position().top + 117,
+                'left': $(this).position().left + 117
+            });
+            $(this).find(".panneauTxt").show();
+            $(this).animate({ width: '470px' });
+            $(this).addClass("central");
+            $(this).removeClass("imgG");
         }
     });
 });
