834a582771fa02c248cda9b77e3fbdf6a239c8c2
lordrayd <samuel-leb@hotmail.fr>
2020-03-11 16:15:26 +0100

    update js

--- 2244_Programmation_Web/TP7/script.js
+++ 2244_Programmation_Web/TP7/script.js
@@ -1,79 +1,52 @@
 $(document).ready(function() {
-    $("#pression").click(function() {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                var plotly_vars = JSON.parse(this.responseText);
-                console.log(plotly_vars['heure'])
-            }
-        };
-        xmlhttp.open("GET", "http://localhost:3000/pression", true);
-        xmlhttp.send();
+    $.get("http://localhost:3000/news", function(data, status) {
+        $("#news").html(data);
     });
-    $("#temp").click(function() {
-        var pression = {};
-        var temperature = {};
-
-
-        var xmlhttpPression = new XMLHttpRequest();
-        xmlhttpPression.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                let plotly_vars = JSON.parse(this.responseText);
-                console.log(plotly_vars['heure']);
-                pression = {
-                    name: "Air pressure",
-                    x: plotly_vars['heure'],
-                    y: [81.32, 72.1, 121.41, 71.31, 40.98, 30.96, 51.2, 71.92],
-                    text: ['81.32 Pa', '72.1 Pa', '121.41 Pa', '71.31 Pa', '40.98 Pa', '30.96 Pa', '51.2 Pa', '71.92 Pa'],
-                    hoverinfo: 'x+text+name',
-                    type: "line",
-                };
-            }
+    $.getJSON("http://localhost:3000/pression", function(data, status) {
+        var pression = {
+            name: "Pression",
+            x: data.heure,
+            y: data.pression,
+            type: "line"
         };
-        xmlhttpPression.open("GET", "http://localhost:3000/pression", true);
-        xmlhttpPression.send();
-
-
-        var xmlhttpTemp = new XMLHttpRequest();
-        xmlhttpTemp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                let plotly_vars = JSON.parse(this.responseText);
-                temperature = {
-                    name: "Energy consumption",
-                    x: plotly_vars['heure'],
-                    y: [20, 50, 92, 82, 14, 20, 50, 62],
-                    text: ['20 kw/H', '50 kw/H', '92 kw/H', '82 kw/H', '14 kw/H', '20 kw/H', '50 kw/H', '62 kw/H'],
-                    hoverinfo: 'x+text+name',
-                    type: "bar",
-                    marker: {
-                        color: '#270657'
-                    }
-                };
-            }
-        };
-        xmlhttpTemp.open("GET", "http://localhost:3000/temp", true);
-        xmlhttpTemp.send();
-
-        var data = [temperature, pression];
-
-        layout = {
-            showlegend: false,
-            yaxis2: {
-                type: "category",
-                side: "right",
-                overlaying: "y"
-            }
-        };
-        Plotly.newPlot('plotly-chart', data, layout);
-
+        $.getJSON("http://localhost:3000/temp", function(data, status) {
+            var temperature = {
+                name: "Temperature",
+                x: data.heure,
+                y: data.temp,
+                type: "line"
+            };
+            var data = [temperature, pression];
+            console.log(data);
+            Plotly.newPlot('plotly-chart', data);
+        });
     });
-    $("#newsButton").click(function() {
-
-    });
-
     setInterval(function() {
         $.get("http://localhost:3000/news", function(data, status) {
             $("#news").html(data);
         });
+    }, 10000);
+    setInterval(function() {
+        $.getJSON("http://localhost:3000/pression", function(data, status) {
+            var pression = {
+                name: "Pression",
+                x: data.heure,
+                y: data.pression,
+                type: "line"
+            };
+            $.getJSON("http://localhost:3000/temp", function(data, status) {
+                var temperature = {
+                    name: "Temperature",
+                    x: data.heure,
+                    y: data.temp,
+                    type: "line"
+                };
+                var data = [temperature, pression];
+                console.log(data);
+                Plotly.newPlot('plotly-chart', data);
+            });
+        });
     }, 4000);
+
+    $("#plotly-chart")
 });
