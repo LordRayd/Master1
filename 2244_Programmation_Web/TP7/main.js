15b5a4f45f904ddd4b2e26a51ba3140a9da73215
LordRayd <samuel-leb@hotmail.fr>
2020-03-15 20:45:59 +0100

    update error jslint

--- 2244_Programmation_Web/TP7/main.js
+++ 2244_Programmation_Web/TP7/main.js
@@ -1,10 +1,10 @@
 // mini serveur web pour le TD Ajax
 // G.Ménier UBS
 
-const http = require('http')
-const URL = require('url')
-const FS = require('fs')
-const port = 3000
+const http = require('http');
+const URL = require('url');
+const FS = require('fs');
+const port = 3000;
 
 // faux news feed
 const msg = [
@@ -13,44 +13,44 @@ const msg = [
     "Chutes de meteorites a prevoir ce soir : sortez avec des parapluies en titane",
     "Le maire de Vannes declare : 'NguZobut'. Notre decryptage en page centrale",
     "Penurie de glace au cassis"
-]
+];
 
 // tableaux de données
-tempTab = []
-tempCourante = 45
+tempTab = [];
+tempCourante = 45;
 
-pressionTab = []
-pressCourante = 1200
+pressionTab = [];
+pressCourante = 1200;
 
 // repère commun pour les données
-heureTab = []
-heureCourante = 0
+heureTab = [];
+heureCourante = 0;
 
 // production des données
 function productionDonnees() {
-    heureCourante = heureCourante + 1
+    heureCourante = heureCourante + 1;
 
-    tempCourante = tempCourante + 1 - 2 * Math.random() * 5
-    pressCourante = pressCourante + 1 - 2 * Math.random() * 50
+    tempCourante = tempCourante + 1 - 2 * Math.random() * 5;
+    pressCourante = pressCourante + 1 - 2 * Math.random() * 50;
 
-    pressionTab.push(pressCourante)
-    tempTab.push(tempCourante)
-    heureTab.push(heureCourante)
+    pressionTab.push(pressCourante);
+    tempTab.push(tempCourante);
+    heureTab.push(heureCourante);
 }
 
-setInterval(productionDonnees, 1000)
+setInterval(productionDonnees, 1000);
 
 // callback de réaction
 const requestHandler = (requete, reponse) => {
-    let ulp = URL.parse(requete.url)
-    let maintenant = new Date()
-    console.log("requete : " + ulp.pathname)
+    let ulp = URL.parse(requete.url);
+    let maintenant = new Date();
+    console.log("requete : " + ulp.pathname);
     switch (ulp.pathname) {
 
 
         case '/news': // news feed
-            reponse.write(msg[Math.floor(5 * Math.random())])
-            reponse.end()
+            reponse.write(msg[Math.floor(5 * Math.random())]);
+            reponse.end();
             break;
 
         case '/temp': // températures
@@ -59,8 +59,8 @@ const requestHandler = (requete, reponse) => {
                 heure: heureTab,
                 temp: tempTab,
                 quand: maintenant.toLocaleString()
-            }), encoding = 'utf8')
-            reponse.end()
+            }), encoding = 'utf8');
+            reponse.end();
             break;
 
         case '/pression': // pressions
@@ -69,23 +69,25 @@ const requestHandler = (requete, reponse) => {
                 heure: heureTab,
                 pression: pressionTab,
                 quand: maintenant.toLocaleString()
-            }), encoding = 'utf8')
-            reponse.end()
+            }), encoding = 'utf8');
+            reponse.end();
             break;
 
 
         default:
-            let rq = ulp.pathname
-            if (rq.length > 1) rq = rq.slice(1);
+            let rq = ulp.pathname;
+            if (rq.length > 1) {
+                rq = rq.slice(1);
+            }
             else rq = "index.html"
             FS.readFile(rq, (err, contents) => {
 
                 if (!err) {
-                    reponse.end(contents)
+                    reponse.end(contents);
                 } else {
                     reponse.writeHead(404, { 'Content-Type': 'text/html' });
-                    reponse.write("Probleme de lecture d'index.html !!")
-                    reponse.end()
+                    reponse.write("Probleme de lecture d'index.html !!");
+                    reponse.end();
                 };
             });
             break;
@@ -96,12 +98,12 @@ const requestHandler = (requete, reponse) => {
 
 
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
     if (err) {
-        return console.log('Probleme : ', err)
+        return console.log('Probleme : ', err);
     }
 
-    console.log(`Le mini serveur est en ligne sur le port : ${port}`)
+    console.log(`Le mini serveur est en ligne sur le port : ${port}`);
 })
